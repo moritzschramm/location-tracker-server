56c0b606ad92f8e29d044548a440a37daaff1025
Moritz Schramm <moritz@moritz-schramm.com>
2019-03-16 18:54:21 +0100

    implement location controller

--- controller/location_controller.go
+++ controller/location_controller.go
@@ -1,3 +1,40 @@
 package controller
 
-import ()
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/moritzschramm/location-tracker-server/model"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type LocationController struct {
+	DB *sql.DB
+}
+
+func (controller *LocationController) GetLocations(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+
+	from, err := time.Parse(time.RFC3339, params.ByName("from"))
+	if err != nil {
+		http.Error(res, "'from' parameter malformed", 400)
+	}
+
+	to, err := time.Parse(time.RFC3339, params.ByName("to"))
+	if err != nil {
+		http.Error(res, "'to' parameter malformed", 400)
+	}
+
+	locations := model.GetLocations(controller.DB, from, to)
+
+	locationsJson, err := json.Marshal(locations)
+	if err != nil {
+		log.Println("Error retrieving locations: ", err)
+		http.Error(res, "Internal Server Error", 500)
+		return
+	}
+
+	res.Write(locationsJson)
+}
