a3651a102e86cbdf21349f295099bc180eb92c3e
Moritz Schramm <moritz@moritz-schramm.com>
2019-03-25 18:10:38 +0100

    add struct for control settings

--- model/control_settings.go
+++ model/control_settings.go
@@ -1,3 +1,13 @@
 package model
 
-import ()
+import (
+	"database/sql"
+	"time"
+)
+
+type ControlSettings struct {
+	DB       *sql.DB   `json:"-"`
+	Id       int       `json:"-"`
+	DeviceId int       `json:"-"`
+	UpdateAt time.Time `json:"updatedAt"`
+}
